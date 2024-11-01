70c0573c233180547143acd2d55a17bc6be4de30
Miguel Andres Herize Godoy <mherize@ia.com.mx>
2024-11-01 02:04:48 -0400

    add webscrapy FlipcolivingPipeline

---
+++ src/app/scrapy/flipcoliving/flipcoliving/flipcoliving/pipelines.py
@@ -0,0 +1,308 @@
+# -*- coding: utf-8 -*-
+
+import json
+import os
+import re
+from os import path
+import logging
+from .constants import spider_names
+from pydantic import BaseModel
+import unicodedata
+from enum import Enum
+from typing import Union, List
+import app.utils.constants as constants
+from app.models.enums import feature_map, CurrencyCode, PaymentCycleEnum, Languages
+from app.models.schemas import (
+    Property,
+    RentalUnits,
+    ContractModel,
+    Description,
+    Image,
+    LocationAddress,
+    DatePayload,
+    DatePayloadItem
+)
+from app.utils.funcs import find_feature_keys, get_elements_types, save_property, save_rental_unit,get_month_dates,check_and_insert_rental_unit_calendar,detect_language
+
+
+os.makedirs("logs/flipcoliving", exist_ok=True)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler(
+            os.path.join("logs/flipcoliving", "spider.log"), mode="a", encoding="utf-8"
+        ),
+    ],
+)
+logger = logging.getLogger(__name__)
+
+class RoomData(BaseModel):
+    areaM2: int
+    amount: int
+    schedule: str
+    title: str
+    images: List[str]
+
+def remove_accents(text: str) -> str:
+    normalized_text = unicodedata.normalize('NFD', text)
+    return ''.join(char for char in normalized_text if unicodedata.category(char) != 'Mn')
+
+def get_all_imagenes(space_images: list) -> list[dict]:
+    all_imagenes = []
+
+    if not space_images:
+        return []
+    for index, value in enumerate(space_images):
+        cover = True if index == 0 else False
+        all_imagenes.append(
+            {
+                "image": value,
+                "isCover": cover,
+            }
+        )
+    return all_imagenes
+
+
+
+def get_all_descriptions(parse_description: list):
+    all_descriptions = []
+    dict_language = {
+        Languages.SPANISH.value: "Propiedades",
+        Languages.ENGLISH.value: "Property",
+    }
+    
+    for description in parse_description:
+        lang_id = detect_language(description)  # Detecta el idioma de la descripción
+        if not lang_id:
+            continue  # Salta en caso de que no se detecte el idioma
+        
+        descriptions = {
+            "LanguagesId": lang_id,
+            "title": dict_language.get(lang_id, ""),
+            "description": remove_accents(description),
+        }
+        
+        if descriptions["description"]:  # Verifica que la descripción no esté vacía
+            all_descriptions.append(descriptions)
+
+    return all_descriptions
+
+def create_json(item: Union[RentalUnits, Property, DatePayloadItem]) -> None:
+    class PathDocument(Enum):
+        PROPERTY = "data/property.json"
+        RENTAL_UNITS = "data/rental_units.json"
+        CALENDAR = "data/calendar.json"
+
+    current_dir = os.getcwd()
+
+    if isinstance(item, RentalUnits):
+        json_file_path = os.path.join(current_dir, PathDocument.RENTAL_UNITS)
+    elif isinstance(item, Property):
+        json_file_path = os.path.join(current_dir, PathDocument.PROPERTY)
+    elif isinstance(item, DatePayloadItem):
+        json_file_path = os.path.join(current_dir, PathDocument.CALENDAR)
+    else:
+        raise ValueError("item must be an instance of RentalUnits or Property or Calendar.")
+
+    os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
+
+    with open(json_file_path, "a", encoding="utf-8-sig") as json_file:
+        json.dump(item.dict(), json_file, indent=4)
+
+    logging.info(f"Datos guardados en: {json_file_path}")
+
+
+def create_rental_units(
+    property_data: Property, bedroom_count: int, rooms_data: List[RoomData]
+) -> List[RentalUnits]:
+    rental_units = []
+    calendar_unit_list = []
+
+    if bedroom_count < 1:
+        bedroom_count = 1
+
+    if rooms_data is None:
+        rooms_data = []
+
+    if not rooms_data:
+        rooms_data = [RoomData(areaM2=int(property_data.areaM2), amount=0, schedule="Default", title="Default", images=[''])]  # Ajusta según los atributos de RoomData
+
+    rooms_per_type = bedroom_count // len(rooms_data)
+
+    for _, room_data in enumerate(rooms_data):
+        for unit_index in range(rooms_per_type):
+            reference_code = (
+                f"{room_data.title}-{unit_index + 1:03}"
+            )
+            reference_code = re.sub(r'\s', '-', reference_code)
+            rental_unit = RentalUnits(
+                PropertyId=property_data.id,
+                referenceCode=reference_code,
+                areaM2=int(room_data.areaM2),
+                Features=property_data.Features,
+                isActive=True,
+                isPublished=True,
+                ContractsModels=[
+                    ContractModel(
+                        PropertyBusinessModelId=get_elements_types(
+                            constants.MODELS_CONTRACT
+                        ),
+                        currency=CurrencyCode.EUR.value,
+                        amount=int(room_data.amount),
+                        depositAmount=constants.INT_ZERO,
+                        reservationAmount=constants.INT_ZERO,
+                        minPeriod=constants.INT_ONE,
+                        paymentCycle=PaymentCycleEnum.MONTHLY.value,
+                        extras=[],
+                    )
+                ],
+                Descriptions=property_data.Descriptions,
+                Images=get_all_imagenes(room_data.images),
+            )
+
+            rental_units.append(rental_unit)
+            # Calendar
+            month_year = room_data.schedule.split("from")[-1].strip()
+            start_date, end_date = get_month_dates(room_data.schedule)
+            date_items=DatePayloadItem(
+                summary= f"Blocked until {month_year}",
+                description= room_data.schedule,
+                startDate= start_date,
+                endDate= end_date,
+            )
+            calendar_unit_list.append(date_items)
+
+    return rental_units, calendar_unit_list
+
+
+def get_default_values() -> dict:
+    """
+    Retorna un diccionario con los valores por defecto (hardcoded).
+    """
+    return {
+        "cancellationPolicy": constants.CANCELLATION_POLICY,
+        "rentalType": constants.RENTAL_TYPE,
+        "isActive": constants.BOOL_TRUE,
+        "isPublished": constants.BOOL_TRUE,
+        "Languages": constants.LANGUAGES,
+        "PropertyTypeId": get_elements_types(constants.PROPERTY_TYPE_ID),  # 4
+        "country": constants.COUNTRY,
+        "countryCode": constants.COUNTRY_CODE,
+    }
+
+
+
+def parse_banner_features(banner_features):
+    location = banner_features[0]
+
+    price_info = banner_features[1]
+    max_price = "".join(filter(str.isdigit, price_info.split("to")[-1]))
+    currency = "".join(filter(str.isalpha, price_info.split()[1]))
+    sqm = "".join(filter(str.isdigit, banner_features[2]))
+    bedrooms = int("".join(filter(str.isdigit, banner_features[3]))) if len(banner_features) > 3 else 1
+
+    return location, int(max_price), currency, int(sqm), int(bedrooms)
+
+
+class FlipcolivingPipeline:
+    def open_spider(self, spider):
+        # Create file path for JSON output
+        self.json_path = path.join(spider.output_folder, spider.output_filename)
+        # Initialize a list to hold items
+        self.items = []
+
+    def process_item(self, item, spider):
+        # Add item to the list
+        self.items.append(dict(item))  # Convert item to dict if it's not already
+
+        # save lodgerin
+        defaults = get_default_values()
+        item = item["items_output"]
+        neighborhood, amount, currency, areaM2, bedrooms = parse_banner_features(
+            item["banner_features"]
+        )
+
+        result_description = get_all_descriptions(item["parse_description"])
+        parse_coliving_name = remove_accents(item["parse_coliving_name"][0]).replace(" ", "-")
+        logger.info(f"result_description {result_description}")
+
+        try:
+            property_item = Property(
+                name=parse_coliving_name,
+                description=item["parse_description"][0],
+                referenceCode=f'{item["city_name"]}-{parse_coliving_name}-001',
+                areaM2=areaM2,
+                rentalType=defaults["rentalType"],
+                isActive=defaults["isActive"],
+                isPublished=defaults["isPublished"],
+                Features=find_feature_keys(item["all_features"], feature_map),
+                tourUrl=(
+                    item["tour_url"][0]
+                    if "tour_url" in item and item["tour_url"]
+                    else None
+                ),
+                PropertyTypeId=defaults["PropertyTypeId"],
+                Descriptions=[
+                    Description(
+                        LanguagesId=data_description["LanguagesId"],
+                        title=data_description["title"],
+                        description=data_description["description"],
+                    ).dict()
+                    for data_description in result_description
+                ],
+                Images=item.get("all_firts_imagenes", []),
+                Location=LocationAddress(
+                    lat=item["latitude"][0],
+                    lon=item["longitude"][0],
+                    country=defaults["country"],
+                    countryCode=defaults["countryCode"],
+                    city=item["city_name"],
+                ),
+            )
+
+            logger.info(f"property_item creado con éxito: {property_item}")
+            property_id = save_property(property_item)
+            property_item.id = property_id
+            rooms_data = [
+                RoomData(
+                    areaM2=int(rental_unit['data_rental_unit'][1].replace(" sqm", "")),
+                    amount=int(rental_unit['data_rental_unit'][0].split(" to ")[-1].replace("€ /month", "").strip()),
+                    schedule=rental_unit['available_rental_unit'][0],
+                    title=remove_accents(rental_unit['name_rental_unit'][0].replace(" ", "_")), 
+                    images=rental_unit['imagenes_rental_unit'], 
+                )
+                for rental_unit in item['rental_units']
+            ]
+            rental_units_items, calendar_unit_list = create_rental_units(
+                    property_item, 
+                    int(bedrooms), 
+                    rooms_data
+                )
+            list_rental_unit_id = []
+            for unit in rental_units_items:
+                rental_unit = save_rental_unit(unit)
+                unit.id = rental_unit
+                list_rental_unit_id.append(rental_unit)
+
+            # for unit in rental_units_items:
+                # create_json(unit)
+            logger.info(f'list_rental_unit_id {list_rental_unit_id}')
+            #schedule
+            for rental_id, calendar_unit in zip(list_rental_unit_id, calendar_unit_list):
+                check_and_insert_rental_unit_calendar(rental_id, calendar_unit)
+
+            # for calendar_unit in calendar_unit_list:
+                # create_json(calendar_unit)
+
+            return property_item
+
+        except Exception as e:
+            logger.info(f"Error al iniciar el scraping para {str(e)}")
+
+    def close_spider(self, spider):
+        # Write the collected items to a JSON file
+        with open(self.json_path, "a", encoding="utf-8") as json_file:
+            json.dump(self.items, json_file, ensure_ascii=False, indent=4)
---
+++ src/app/scrapy/flipcoliving/flipcoliving/flipcoliving/spiders/flipcoliving_spider.py
@@ -0,0 +1,350 @@
+# coding=utf-8
+import os
+import re
+import scrapy
+import logging
+import requests
+
+from os import path
+from pathlib import Path
+from scrapy import Selector
+
+from .. import items
+from ..constants import item_custom_settings, item_input_output_archive
+from ..enum_path import XpathGeneralColiving
+
+os.makedirs('logs/flipcoliving', exist_ok=True)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler(
+            os.path.join('logs/flipcoliving', "spider.log"), mode="a", encoding="utf-8"
+        ),
+    ],
+)
+logger = logging.getLogger(__name__)
+
+# scrapy crawl flipcoliving_spider -a output_folder_path= -a output_folder_name= -a file_name= -a processed_name=
+# scrapy crawl flipcoliving_spider
+
+class FlipcolivingSpiderSpider(scrapy.Spider):
+    name = "flipcoliving_spider"
+    custom_settings = item_custom_settings
+    
+    def __init__(self, *args, **kwargs):
+
+        super(FlipcolivingSpiderSpider, self).__init__(*args, **kwargs)
+
+        # -----------------------------------------------------------------
+        # Ruta de la carpeta donde se almancera la data extraida
+        self.output_folder_path = kwargs.pop(
+            'output_folder_path', item_input_output_archive['output_folder_path']
+        )
+        # Nombre de la carpeta donde se almacenara la data extraida
+        self.output_folder_name = kwargs.pop(
+            'output_folder_name', item_input_output_archive['output_folder_name']
+        )
+        # Path de la carpeta salida
+        self.output_folder = path.join(
+            self.output_folder_path, self.output_folder_name
+        )
+
+        # -----------------------------------------------------------------
+        # Nombre del documento con la data extraida sin refinar
+        self.output_filename = kwargs.pop(
+            'file_name', item_input_output_archive['file_name']
+        )
+        # Nombre del documento con la data extraida refinada
+        self.output_filename_processed = kwargs.pop(
+            'processed_name', item_input_output_archive['processed_name']
+        )
+        
+        # -----------------------------------------------------------------
+        # if folder not exists create one
+        Path(self.output_folder).mkdir(
+            parents=True, exist_ok=True
+        )
+
+
+    def start_requests(self):
+
+        """
+        Inicio de la pagina principal
+        """
+
+        url = 'https://flipcoliving.com/'
+        
+        yield scrapy.Request(
+            url=url,
+        )
+
+    def parse(self, response):
+
+        """
+        Obtener las URL de las Ciudades
+        """
+
+        cities_url = response.xpath(XpathGeneralColiving.CITIES_URL.value)
+
+        if not cities_url:
+            logger.warning('No se obtuvieron las URL de las ciudades')
+            return None
+        
+        # Recorrer ciudades
+        for city_url in cities_url:
+            
+            aux_city_url = city_url.get()
+            aux_city_url = response.url + aux_city_url
+            if aux_city_url is None:
+                logger.warning('Se obtuvo un valor para la url de la ciudad: %s', aux_city_url)
+                continue
+
+            city_name = re.sub(r"[^A-Za-z]", " ", aux_city_url.split("-")[-1]).strip()
+
+            logger.info("Parseando -> ciudad: %s. url: %s", city_name, aux_city_url)
+            yield scrapy.Request(
+                url=aux_city_url,
+                dont_filter=True,
+                meta={
+                    "city_name": city_name,
+                    "aux_city_url": aux_city_url,
+                },
+                callback=self.parse_all_colivings
+            )
+
+
+    def parse_all_colivings(self, response):
+
+        """
+        Obtener las URL de los coliving
+        """
+
+        colivings_url = response.xpath(XpathGeneralColiving.COLIVING_URL.value)
+
+        if not colivings_url:
+            logger.warning('No se obtuvieron las URL de los colivings')
+            return None
+
+        items_meta = response.meta
+        
+        # Recorrer los Coliving
+        for coliving_url in colivings_url:
+            
+            coliving_url = coliving_url.get()
+
+            if coliving_url is None:
+                logger.warning('Se obtuvo un valor para el coliving: %s', coliving_url)
+                continue
+
+            coliving_name = re.sub(r"[^A-Za-z]", " ", coliving_url.split("/")[-2]).strip()
+
+            logger.info("Parseando -> URL Coliving: %s", coliving_url)
+
+            yield scrapy.Request(
+                url=coliving_url,
+                dont_filter=True,
+                meta={
+                    "city_name": items_meta['city_name'],
+                    "aux_city_url": items_meta['aux_city_url'],
+                    "coliving_url": coliving_url,
+                    "coliving_name": coliving_name,
+                },
+                callback=self.parse_coliving
+            )
+
+
+    def parse_coliving(self, response):
+
+        """
+        Extraer la data del coliving
+        """
+
+        items_meta = response.meta
+
+        # ------------------------------------------------------------
+
+        items_output = {
+    
+            # Extraccion de las imagenes principales
+            'all_firts_imagenes': XpathGeneralColiving.FIRST_IMAGENES.value,
+    
+            # Extraccion del nombre del Coliving
+            'parse_coliving_name': XpathGeneralColiving.COLIVING_NAME.value,
+    
+            # Extraccion del Banner: Ubicacion, rango de precio, area, habitaciones
+            'banner_features': XpathGeneralColiving.BANNER_FEATURES.value,
+    
+            # Extraccion de la descriccion
+            'parse_description': XpathGeneralColiving.ABOUT_THE_HOME.value,
+        
+            # Extraccion de los features
+            'all_features': XpathGeneralColiving.FEATURES.value,
+
+            # Extraccion de las URL tours
+            'all_url_tour': XpathGeneralColiving.TOUR_URL.value,
+            
+            # Extraccion de las URL tours
+            'latitude': XpathGeneralColiving.LATITUDE.value,
+
+            # Extraccion de las URL tours
+            'longitude': XpathGeneralColiving.LONGITUDE.value,
+        }
+
+        for key, value_data_1 in items_output.items():
+            items_output[key] = response.xpath(value_data_1).getall()
+            if items_output[key]:
+                items_output[key] = self.check_data_object(items_output[key])
+        items_output['all_firts_imagenes'] = self.remove_duplicate_urls(items_output['all_firts_imagenes'])
+        items_output['all_firts_imagenes'] = self.get_all_imagenes(items_output['all_firts_imagenes'])
+
+        items_output["city_name"] = items_meta["city_name"]
+        items_output["aux_city_url"] = items_meta["aux_city_url"]
+        items_output["coliving_url"] = items_meta["coliving_url"]
+        items_output["coliving_name"] = items_meta["coliving_name"]
+        # ------------------------------------------------------------
+        # Extraccion informacion: The rooms and The Unit
+
+        items_rental_units = []
+
+        type_name_rental_unit = response.xpath(XpathGeneralColiving.TYPE_NAME_RENTAL_UNIT.value).get()
+
+        if type_name_rental_unit == 'The Unit':
+            # Unico rental unit
+
+            data_rental_unit = {
+                # tipo de rental unit
+                "type_name_rental_unit": [type_name_rental_unit],
+    
+                # Nombre del rental unit
+                "name_rental_unit": response.xpath(XpathGeneralColiving.NAME_RENTAL_UNIT.value).getall(),
+    
+                # Estado del rental unit
+                "available_rental_unit": response.xpath(XpathGeneralColiving.AVAILABLE_RENTAL_UNIT.value).getall(),
+    
+                # Rango de precio y are del rental unit
+                "data_rental_unit": response.xpath(XpathGeneralColiving.DATA_RENTAL_UNIT.value).getall(),
+    
+                # Extraccion de las imagenes del rental unit
+                "imagenes_rental_unit": response.xpath(XpathGeneralColiving.IMAGENES_RENTAL_UNIT.value).getall(),
+            }
+            data_rental_unit['imagenes_rental_unit'] = self.remove_duplicate_urls(data_rental_unit['imagenes_rental_unit'])
+
+            items_rental_units.append(data_rental_unit)
+
+        elif type_name_rental_unit == 'The Rooms':
+            # Multiples rental unit
+
+            all_rental_unit = response.xpath("//div[contains(@class, 'theRooms__slider')]/div")
+
+            if not all_rental_unit:
+                items_rental_units.append({})
+            else:
+                for value_data_2 in all_rental_unit:
+
+                    data_rental_unit = {
+                        # tipo de rental unit
+                        "type_name_rental_unit": [type_name_rental_unit],
+            
+                        # Nombre del rental unit
+                        "name_rental_unit": value_data_2.xpath(XpathGeneralColiving.MULTIPLE_NAME_RENTAL_UNIT.value).getall(),
+            
+                        # Estado del rental unit
+                        "available_rental_unit": value_data_2.xpath(XpathGeneralColiving.MULTIPLE_AVAILABLE_RENTAL_UNIT.value).getall(),
+            
+                        # Rango de precio y are del rental unit
+                        "data_rental_unit": value_data_2.xpath(XpathGeneralColiving.MULTIPLE_DATA_RENTAL_UNIT.value).getall(),
+            
+                        # Extraccion de las imagenes del rental unit
+                        "imagenes_rental_unit": value_data_2.xpath(XpathGeneralColiving.MULTIPLE_IMAGENES_RENTAL_UNIT.value).getall(),
+                    }
+                    data_rental_unit['imagenes_rental_unit'] = self.remove_duplicate_urls(data_rental_unit['imagenes_rental_unit'])
+
+                    items_rental_units.append(data_rental_unit)
+
+        else:
+            logger.info('No presenta rental_units el coliving: %s. Ciudad: %s. URL: %s', items_meta['coliving_name'], items_meta['city_name'], response.url)
+
+        items_rental_units = self.check_data_object(items_rental_units)
+        all_url = response.xpath(XpathGeneralColiving.LANGUAGE_URL.value).getall()
+
+        items_output['rental_units'] = items_rental_units
+
+        for index_language, url_language in enumerate(all_url):
+            
+            if index_language == 0:
+                # ingles
+                continue
+
+            description = self.parse_coliving_language(url_language)
+            items_output['parse_description'].append(description)
+
+        item = items.FlipcolivingItem()
+        item['items_output'] = items_output
+        yield item
+
+    def parse_coliving_language(self, url):
+        response_with_requests = requests.get(url)
+        response_with_scrapy = Selector(text=response_with_requests.text)
+        description = response_with_scrapy.xpath(XpathGeneralColiving.ABOUT_THE_HOME.value).get()
+        return description
+
+
+    def check_data_object(self, data_object: list) -> list:
+
+        # Utilizado para los Rental Units
+        if isinstance(data_object[0], dict):
+            
+            # Recorrer todos los rental units
+            for index, rental_unit in enumerate(data_object):
+                rental_unit: dict
+                for key, value_data in rental_unit.items():
+                    data_object[index][key] = list(map(self.check_data, value_data))
+                    data_object[index][key] = list(filter(None, data_object[index][key]))
+            return data_object
+
+        # Solo listas
+        if isinstance(data_object[0], str):
+            data_object = list(map(self.check_data, data_object))
+            data_object = list(filter(None, data_object))
+            return data_object
+                        
+
+    def check_data(self, data) -> list:
+
+        data = re.sub(r'\t|\n|\r|\xa0', '  ', data).strip()
+        data = re.sub(r'\s{2,}', ' ', data).strip()
+        return data
+    
+
+    def remove_duplicate_urls(self, url_list: list) -> list:
+        cleaned_urls = []
+        seen = set()
+
+        for url in url_list:
+            clean_url = re.sub(r"-\d+x\d+\.jpg", ".jpg", url.split()[0])
+
+            if clean_url not in seen:  # If the item has not been seen
+                seen.add(clean_url)  # Add it to the set
+                cleaned_urls.append(clean_url)  # Add it to the result list
+
+        return cleaned_urls
+    
+
+    def get_all_imagenes(self,space_images: list) -> list[dict]:
+        all_imagenes = []
+
+        if not space_images:
+            return []
+        for index, value in enumerate(space_images):
+            cover = True if index == 0 else False
+            all_imagenes.append(
+                {
+                    "image": value,
+                    "isCover": cover,
+                }
+            )
+        return all_imagenes
+
